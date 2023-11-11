923c7551fba9581111020f80fded1d3db88f3479
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-11 08:23:28 -0800

    AutoPush

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <ctype.h>
+
+int _printf(const char *format, ...);
+
+#endif
