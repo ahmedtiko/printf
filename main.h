977e68ce8aeac0df7a69a96592774bbaf6e82d0c
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-11 10:06:33 -0800

    AutoPush

--- main.h
+++ main.h
@@ -10,9 +10,10 @@
 #include <unistd.h>
 #include <ctype.h>
 
-
+/* task 4 */
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
+
 #define NULL_STRING "(null)"
 #define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 #define CONVERT_LOWERCASE 1
@@ -22,10 +23,15 @@ typedef struct parameters
 {}
 
 /* task 0 functions */
-
 int print_char(va_list ap, params_t *params);
-int print_string(va_list ap, params_t params);
-int print_percent(va_list ap, params_t params);
+int print_string(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+
+/* task 1 functions */
+int print_int(va_list ap, params_t *params);
+
+/* task 2 functions */
+int print_binary(va_list ap, params_t *params);
 
 int _printf(const char *format, ...);
 int _putchar(char c);
